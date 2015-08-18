38fa676a17080c84df984b7139a017ea5d51457d
jackdalton <jackdalton00@gmail.com>
2015-08-18 16:34:27 -0600

    Migrated to Visual Studio project.

--- src/flib.h
+++ src/flib.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <math.h>
 #include <string>
 #include <sstream>
@@ -8,59 +10,62 @@ int F_DECIMAL = 2;
 
 class Fraction {
 public:
-    Fraction(double numerator = 0, double denominator = 1) {
-        n = numerator;
-        d = denominator;
-    }
-    double get(int target) {
-        if (target == 0) {
-            return n;
-        } else if (target == 1) {
-            return d;
-        } else if (target == 2) {
-            return n / d;
-        }
-    }
-    Fraction reduce() {
-        double ld = d;
-        double ln = n;
-        for (int i = d * n; i > 1; i--) {
-            if ((fmod(d, i) == 0) && (fmod(n, i) == 0)) {
-                ln /= i;
-                ld /= i;
-            }
-        }
-        return Fraction(ln, ld);
-    }
-    bool greaterThan(Fraction f) {
-        if (get(F_DECIMAL) > f.get(F_DECIMAL)) {
-            return true;
-        }
-        return false;
-    }
-    bool lessThan(Fraction f) {
-        return !greaterThan(f);
-    }
-    bool equalTo(Fraction f) {
-        if (f.get(F_DECIMAL) == get(F_DECIMAL)) {
-            return true;
-        }
-        return false;
-    }
-    std::string toString() {
-        std::stringstream st;
-        st << n << "/" << d;
-        return st.str();
-    }
-    bool set(double numerator = 0, double denominator = 1) {
-        if (denominator != 0) {
-            n = numerator;
-            d = denominator;
-            return true;
-        } else {
-            return false;
-        }
-    }
+	Fraction(double numerator = 0, double denominator = 1) {
+		n = numerator;
+		d = denominator;
+	}
+	double get(int target) {
+		if (target == 0) {
+			return n;
+		}
+		else if (target == 1) {
+			return d;
+		}
+		else if (target == 2) {
+			return n / d;
+		}
+	}
+	Fraction reduce() {
+		double ld = d;
+		double ln = n;
+		for (int i = d * n; i > 1; i--) {
+			if ((fmod(d, i) == 0) && (fmod(n, i) == 0)) {
+				ln /= i;
+				ld /= i;
+			}
+		}
+		return Fraction(ln, ld);
+	}
+	bool greaterThan(Fraction f) {
+		if (get(F_DECIMAL) > f.get(F_DECIMAL)) {
+			return true;
+		}
+		return false;
+	}
+	bool lessThan(Fraction f) {
+		return !greaterThan(f);
+	}
+	bool equalTo(Fraction f) {
+		if (f.get(F_DECIMAL) == get(F_DECIMAL)) {
+			return true;
+		}
+		return false;
+	}
+	std::string toString() {
+		std::stringstream st;
+		st << n << "/" << d;
+		return st.str();
+	}
+	bool set(double numerator = 0, double denominator = 1) {
+		if (denominator != 0) {
+			n = numerator;
+			d = denominator;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 private:
-    double n, d;
+	double n, d;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,29 +2,30 @@
 #include <iostream>
 
 int main() {
-    Fraction f1(1, 2);
-    bool passed = true;
-    if (f1.get(F_NUMERATOR) != 1) passed = false;
-    if (f1.get(F_DENOMINATOR) != 2) passed = false;
-    if (!f1.set(2, 1)) passed = false;
-    if (f1.get(F_NUMERATOR) != 2 ) passed = false;
-    if (f1.get(F_DENOMINATOR) != 1) passed = false;
-    f1.set(1, 2);
-    if (f1.get(F_DECIMAL) != 0.5) passed = false;
-    if (f1.toString() != "1/2") passed = false;
-    f1.set(2, 4);
-    Fraction f2 = f1.reduce();
-    if (f2.get(F_NUMERATOR) != 1) passed = false;
-    if (f2.get(F_DENOMINATOR) != 2) passed = false;
-    if (!f2.equalTo(f1)) passed = false;
-    if (f2.greaterThan(f1)) passed = false;
-    f2.set(2, 3);
-    if (!f2.greaterThan(f1)) passed = false;
-    if (!f1.lessThan(f2)) passed = false;
-    if (passed) {
-        std::cout << "Test passed." << std::endl;
-    } else {
-        std::cout << "Error: Test failed." << std::endl;
-    }
-    return 0;
+	Fraction f1(1, 2);
+	bool passed = true;
+	if (f1.get(F_NUMERATOR) != 1) passed = false;
+	if (f1.get(F_DENOMINATOR) != 2) passed = false;
+	if (!f1.set(2, 1)) passed = false;
+	if (f1.get(F_NUMERATOR) != 2) passed = false;
+	if (f1.get(F_DENOMINATOR) != 1) passed = false;
+	f1.set(1, 2);
+	if (f1.get(F_DECIMAL) != 0.5) passed = false;
+	if (f1.toString() != "1/2") passed = false;
+	f1.set(2, 4);
+	Fraction f2 = f1.reduce();
+	if (f2.get(F_NUMERATOR) != 1) passed = false;
+	if (f2.get(F_DENOMINATOR) != 2) passed = false;
+	if (!f2.equalTo(f1)) passed = false;
+	if (f2.greaterThan(f1)) passed = false;
+	f2.set(2, 3);
+	if (!f2.greaterThan(f1)) passed = false;
+	if (!f1.lessThan(f2)) passed = false;
+	if (passed) {
+		std::cout << "Test passed." << std::endl;
+	}
+	else {
+		std::cout << "Error: Test failed." << std::endl;
+	}
+	return 0;
 }
